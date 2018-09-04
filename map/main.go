76cedcca77611c28ba578f625670a3703c13d0c2
jfkbeatkid <hernadezrigoberto@gmail.com>
2018-09-03 18:16:17 -0700

    for loops are basically the same for structs and maps

--- map/main.go
+++ map/main.go
@@ -5,20 +5,17 @@ import (
 )
 
 func main() {
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"white": "#ffffff",
+	}
 
-	// different ways of declaring a map
-	colors := make(map[string]string)
+	printMap(colors)
+}
 
-	// var colors map [string]string{}
-
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#00ff00",
-	// }
-
-	colors["white"] = "#ffffff"
-
-	delete(colors, "white")
-
-	fmt.Println(colors)
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
