37e63b74f1208816314c57456ebf69d19c488c6c
jfkbeatkid <hernadezrigoberto@gmail.com>
2018-09-03 18:10:34 -0700

    learning methods for interactions with maps

--- map/main.go
+++ map/main.go
@@ -5,10 +5,20 @@ import (
 )
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#00ff00",
-	}
+
+	// different ways of declaring a map
+	colors := make(map[string]string)
+
+	// var colors map [string]string{}
+
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#00ff00",
+	// }
+
+	colors["white"] = "#ffffff"
+
+	delete(colors, "white")
 
 	fmt.Println(colors)
 }
