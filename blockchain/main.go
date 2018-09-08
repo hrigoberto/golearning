0c589401e0e4d69a72a08b949e3f5fb7cbc3b98c
jfkbeatkid <hernadezrigoberto@gmail.com>
2018-09-07 19:32:20 -0700

    creating a basic blockchain and server to run it

---
+++ blockchain/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		t := time.Now()
+		genesisBlock := Block{0, t.String(), 0, "", ""}
+		spew.Dump(genesisBlock)
+		BlockChain = append(BlockChain, genesisBlock)
+	}()
+	log.Fatal(run())
+}
